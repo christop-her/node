e3f133cb650827a18001387139fc4a60e9819cbd
Abstract Programmer <AbstractProgrammer@example.com>
2024-11-27 15:41:35 +0100

    new

--- authController.js
+++ authController.js
@@ -146,24 +146,28 @@ const practitioner_login = async (req, res) => {
 };
 
 
-const transporter = nodemailer.createTransport({
-  service: "gmail",
-  auth: {
-    user: "wilfredc685@gmail.com",
-    pass: "wilfred-124$",
-  },
-});
+
 
 const forgot_password = async (req, res) => {
   try {
 
     const { email } = req.body;
 
+
+
     // Check if the user exists
     const result = await pooll.query('SELECT * FROM patient WHERE email = $1', [email]);
     if (result.rows.length === 0) return res.status(404).send("User not found.");
 
     const user = result.rows[0];
+
+    const transporter = nodemailer.createTransport({
+      service: "gmail",
+      auth: {
+        user: "wilfredc685@gmail.com",
+        pass: "wilfred-124$",
+      },
+    });
 
     // Generate a unique 6-digit code
     const resetCode = Math.floor(100000 + Math.random() * 900000); // 6-digit code
