936591465dcddd2739cd6399c1c8e3c57422c5a0
Abstract Programmer <AbstractProgrammer@example.com>
2024-11-28 17:29:42 +0100

    new

--- authController.js
+++ authController.js
@@ -178,12 +178,6 @@ const forgot_password = async (req, res) => {
     user.resetCode = crypto.createHash("sha256").update(String(resetCode)).digest("hex");
     user.resetCodeExpiry = Date.now() + 15 * 60 * 1000; // Code expires in 15 minutes
 
-     // Update the database with reset code and expiry
-     await pooll.query(
-      "UPDATE patient SET reset_code = $1, reset_code_expiry = $2 WHERE email = $3",
-      [hashedResetCode, resetCodeExpiry, email]
-    );
-
     // Send the code via email
     await transporter.sendMail({
       to: email,
@@ -195,6 +189,12 @@ const forgot_password = async (req, res) => {
     });
     
     res.send("Password reset code sent to your email.");
+
+     // Update the database with reset code and expiry
+     await pooll.query(
+      "UPDATE patient SET reset_code = $1, reset_code_expiry = $2 WHERE email = $3",
+      [hashedResetCode, resetCodeExpiry, email]
+    );
   } catch (error) {
     console.error(error);
     res.status(500).send("An error occurred while sending the reset code.");
